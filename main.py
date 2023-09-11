46063700dd57e24d0174bb395687368650898954
Satvik Ahuja <109898261+satvikahuja@users.noreply.github.com>
2023-09-12 00:16:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
+# import colorgram
+# colors = colorgram.extract('image1.jpg',30)
+# rgb_colors=[]
+#
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#
+#     new_color = (r,g,b)
+#     rgb_colors.append(new_color)
+#
+# print(rgb_colors)
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import turtle as turtle_module
+import random
+turtle_module.colormode(255)
+tim = turtle_module.Turtle()
+tim.penup()
+tim.hideturtle()
+color_list = [(207, 160, 82), (54, 88, 130), (145, 91, 40), (140, 26, 49), (221, 207, 105), (132, 177, 203), (158, 46, 83), (45, 55, 104), (169, 160, 39), (129, 189, 143), (83, 20, 44), (37, 43, 67), (186, 94, 107), (187, 140, 170), (85, 120, 180), (59, 39, 31), (88, 157, 92), (78, 153, 165), (194, 79, 73), (45, 74, 78), (80, 74, 44), (161, 201, 218), (57, 125, 121), (219, 175, 187), (169, 206, 172), (219, 182, 169)]
+
+tim.setheading(225)
+tim.forward(300)
+tim.setheading(0)
+number_of_dots = 100
+for dot_count in range(1,number_of_dots+1):
+    tim.dot(20,random.choice(color_list))
+    tim.forward(50)
+    if dot_count%10==0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+screen = turtle_module.Screen()
+screen.exitonclick()
